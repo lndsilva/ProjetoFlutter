13d85b87cba0afab7843d04086af81aed830b1ee
laercio.nsilva <lndsilva@hotmail.com>
2021-08-27 16:36:00 -0300

    Mudando cabeçalho

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Página de demonstração'),
+      home: MyHomePage(title: 'Hello World - Flutter'),
     );
   }
 }
